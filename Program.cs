3fe22f537569cf2fb672b0369402bce1d39f40d6
Stuart Carruthers <stu@ironiclensflare.com>
2019-09-07 10:11:22 +0100

    Logging

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Text;
+using log4net;
+using rabbit_receiver.log;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -7,19 +9,25 @@ namespace rabbit_receiver
 {
     class Program
     {
+        private static readonly ILog _logger = Logger.GetLogger();
+
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             {
+                _logger.Info("Connecting to RabbitMQ");
                 using (var channel = connection.CreateModel())
                 {
+                    _logger.Debug("Declaring queue");
                     channel.QueueDeclare("samplequeue", true, false, false, null);
+
+                    _logger.Debug("Setting up consumer");
                     var consumer = new EventingBasicConsumer(channel);
                     consumer.Received += (m, i) =>
                     {
                         var message = Encoding.UTF8.GetString(i.Body);
-                        Console.WriteLine($"Received message from queue: {message}");
+                        _logger.Info($"Received message from queue: {message}");
                     };
 
                     channel.BasicConsume("samplequeue", true, consumer);
